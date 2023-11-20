56b69a5ab498d5b26fb399bfd6e7d6e40c3ce943
wise-kim <wisekim@ihopper.co.kr>
2023-11-20 12:24:03 +0900

    Assets

--- src/assets/constant.js
+++ src/assets/constant.js
@@ -1,11 +1,61 @@
-const NEWS_KR = "BTS 정국 '스탠딩 넥스트 투 유' 빌보드 싱글 5위 그룹 방탄소년단 BTS의 정국이 지난 3일 발표한솔로 앨범 타이틀곡으로 미국 빌보드 메인 싱글 차트 '핫 100'에서 5위를 차지했습니다.빌보드는 현지시간 13일 공식 홈페이지를 통해 '스탠딩 넥스트 투 유'가 테일러 스위프트의 '크루얼 서머' 등에 이어 5위에 올랐다고 밝혔습니다. 빌보드는 '정국의 세 번째 솔로 톱 10 기록'이라며 '이 곡은 디지털 송 세일즈에서 1위를 차지했다'고 소개했습니다. '스탠딩 넥스트 투 유'는 정국의 솔로 앨범 '골든'의 타이틀곡으로, 정국의 리듬감 있는 보컬이 돋보이는 레트로 펑크 장르의 곡입니다. 풍성한 관악기 연주와 퍼커션 사운드, 파워풀한 가창이 특징입니다. 정국의 히트곡 '세븐'의 프로듀서 앤드루 와트와 서킷이 참여했습니다. 핫 100은 미국 스트리밍 데이터, 라디오 방송 점수, 판매량 데이터를 종합해 순위가 산출되는 차트로, 빌보드 세부 차트 가운데 가장 의미가 큽니다. '스탠딩 넥스트 투 유'는 1천60만 스트리밍, 40만 라디오 방송 점수, 9만9천 실물ㆍ디지털 싱글 판매량을 기록했습니다. 이 노래는 빌보드 차트 '글로벌 200'과 '글로벌'에서도 각각 1위를 기록했습니다. 정국의 솔로곡 '세븐'과 '3D'는 '글로벌 200'에서 각각 2위와 9위, '글로벌'에서 2위와 5위에 들었습니다."
+const NEWS_KR = "BTS 정국 '스탠딩 넥스트 투 유' 빌보드 싱글 5위\n 그룹 방탄소년단 BTS의 정국이 지난 3일 발표한솔로 앨범 타이틀곡으로 미국 빌보드 메인 싱글 차트 '핫 100'에서 5위를 차지했습니다.\n빌보드는 현지시간 13일 공식 홈페이지를 통해 '스탠딩 넥스트 투 유'가 테일러 스위프트의 '크루얼 서머' 등에 이어 5위에 올랐다고 밝혔습니다.\n 빌보드는 '정국의 세 번째 솔로 톱 10 기록'이라며 '이 곡은 디지털 송 세일즈에서 1위를 차지했다'고 소개했습니다.\n '스탠딩 넥스트 투 유'는 정국의 솔로 앨범 '골든'의 타이틀곡으로, 정국의 리듬감 있는 보컬이 돋보이는 레트로 펑크 장르의 곡입니다.\n 풍성한 관악기 연주와 퍼커션 사운드, 파워풀한 가창이 특징입니다.\n 정국의 히트곡 '세븐'의 프로듀서 앤드루 와트와 서킷이 참여했습니다.\n 핫 100은 미국 스트리밍 데이터, 라디오 방송 점수, 판매량 데이터를 종합해 순위가 산출되는 차트로, 빌보드 세부 차트 가운데 가장 의미가 큽니다.\n '스탠딩 넥스트 투 유'는 1천60만 스트리밍, 40만 라디오 방송 점수, 9만9천 실물ㆍ디지털 싱글 판매량을 기록했습니다.\n 이 노래는 빌보드 차트 '글로벌 200'과 '글로벌'에서도 각각 1위를 기록했습니다.\n 정국의 솔로곡 '세븐'과 '3D'는 '글로벌 200'에서 각각 2위와 9위, '글로벌'에서 2위와 5위에 들었습니다."
 
-const NEWS_JP = "BTS 정국 'スタンディング・ネクスト・トゥ・ユー' ビルボード・シングル5位! グループBTSのジョングクが、先日発表したソロアルバムのタイトル曲で、アメリカのビルボード主要シングルチャート「Hot 100」で5位を獲得しました。ビルボードは現地時間13日、公式ウェブサイトを通じて、「スタンディング・ネクスト・トゥ・ユー」がテイラー・スウィフトの「クルエル・サマー」などに続いて5位に上がったと発表しました。 また、ビルボードは「ジョングクの3番目のソロトップ10記録」と紹介し、「この曲はデジタルソングセールスで1位を獲得した」と述べました。 「スタンディング・ネクスト・トゥ・ユー」はジョングクのソロアルバム「Golden」のタイトル曲であり、ジョングクのリズミカルなボーカルが際立つレトロなパンクジャンルの曲です。 豊かな管楽器の演奏とパーカッションサウンド、パワフルな歌唱が特徴です。 ジョングクのヒット曲「Seven」のプロデューサーであるアンドリュー・ワットとサーキットが参加しました。Hot 100は、アメリカのストリーミングデータ、ラジオ放送スコア、販売量データを総合して順位が算出されるチャートであり、ビルボードの詳細チャートの中で最も意義が大きいです。 「スタンディング・ネクスト・トゥ・ユー」は、1,060万のストリーミング、40万のラジオ放送スコア、99,000の実物・デジタルシングルの売上を記録しました。この曲は、ビルボードのチャート「Global 200」と「Global」でもそれぞれ1位を記録しました。ジョングクのソロ曲「Seven」と「3D」は、「Global 200」でそれぞれ2位と9位、「Global」で2位と5位に入りました。"
+const NEWS_KR2 = "영국 오피셜 차트에 BTS 정국 곡 대거 포함...스트레이 키즈도 첫 진입!\n영국 오피셜 차트에 BTS의 정국의 곡이 대거 올랐습니다.\n현지 시간 17일 공개된 오피셜 싱글 차트 '톱 100'을 보면 정국의 솔로 앨범 '골든'의 타이틀 곡 '스탠딩 넥스트 투 유'가 30위, '투 머치' 48위, '세븐' 54위, '3D' 75위 등 네 곡이 포함됐습니다.\n오피셜 앨범 차트 '톱 100'에도 정국의 솔로 앨범 '골든'은 21위에 올라 있습니다.\n그룹 스트레이 키즈도 새 앨범 '락 스타'의 타이틀 곡 '락'으로 오피셜 싱글 차트 44위에 처음 올랐고 음반은 69위를 기록하고 있습니다.\n세계를 무대로 활동하는 DJ 겸 프로듀서 페기 구의 '(잇 고스 라이크) 나나나'는 70위로 22주째 '톱 100' 자리를 유지하고 있습니다."
 
-const NEWS_ID="BTS Jungkook 'Standing Next to You' mencapai peringkat 5 di tangga lagu Billboard! Anggota grup BTS, Jungkook, meraih peringkat 5 di tangga lagu utama Billboard Hot 100 dengan lagu utama dari album solonya yang dirilis pada tanggal 3. Billboard melalui situs resminya pada tanggal 13 mengumumkan bahwa 'Standing Next to You' naik ke peringkat 5 setelah lagu-lagu seperti 'Cruel Summer' oleh Taylor Swift. Billboard menyebutnya sebagai 'rekam solonya Jungkook yang ketiga yang mencapai 10 besar' dan mengatakan bahwa lagu ini juga meraih peringkat 1 dalam penjualan digital lagu. 'Standing Next to You' menjadi lagu utama dari album solonya yang berjudul 'Golden', lagu ini memiliki genre retro punk dengan vokal Jungkook yang terasa ritmis. Lagu ini memiliki ciri khas dengan permainan alat musik tiup yang kaya dan suara perkusi yang kuat. Produser dari hit Jungkook 'Euphoria', Andrew Watt dan Circuit, juga berpartisipasi dalam lagu ini. Hot 100 adalah tangga lagu yang menggabungkan data streaming, penilaian radio, dan penjualan lagu secara keseluruhan di Amerika Serikat, dan memiliki arti yang besar di antara tangga lagu Billboard lainnya. 'Standing Next to You' mencatatkan 16 juta streaming, 400 ribu penilaian radio, dan 99 ribu penjualan fisik dan digital. Lagu ini juga mencapai peringkat 1 di tangga lagu Billboard Global 200 dan Billboard Global.Lagu solonya Jungkook lainnya, 'Euphoria' dan '3D', menduduki peringkat 2 dan 9 di tangga lagu Billboard Global 200, serta peringkat 2 dan 5 di tangga lagu Billboard Global."
+const NEWS_JP = "BTS ジョングク 'スタンディング・ネクスト・トゥ・ユー' ビルボード・シングル5位!\n グループBTSのジョングクが、先日発表したソロアルバムのタイトル曲で、アメリカのビルボード主要シングルチャート「Hot 100」で5位を獲得しました。\nビルボードは現地時間13日、公式ウェブサイトを通じて、「スタンディング・ネクスト・トゥ・ユー」がテイラー・スウィフトの「クルエル・サマー」などに続いて5位に上がったと発表しました。 また、ビルボードは「ジョングクの3番目のソロトップ10記録」と紹介し、「この曲はデジタルソングセールスで1位を獲得した」と述べました。\n 「スタンディング・ネクスト・トゥ・ユー」はジョングクのソロアルバム「Golden」のタイトル曲であり、ジョングクのリズミカルなボーカルが際立つレトロなパンクジャンルの曲です。\n 豊かな管楽器の演奏とパーカッションサウンド、パワフルな歌唱が特徴です。\n ジョングクのヒット曲「Seven」のプロデューサーであるアンドリュー・ワットとサーキットが参加しました。\nHot 100は、アメリカのストリーミングデータ、ラジオ放送スコア、販売量データを総合して順位が算出されるチャートであり、ビルボードの詳細チャートの中で最も意義が大きいです。\n 「スタンディング・ネクスト・トゥ・ユー」は、1,060万のストリーミング、40万のラジオ放送スコア、99,000の実物・デジタルシングルの売上を記録しました。この曲は、ビルボードのチャート「Global 200」と「Global」でもそれぞれ1位を記録しました。\nジョングクのソロ曲「Seven」と「3D」は、「Global 200」でそれぞれ2位と9位、「Global」で2位と5位に入りました。"
+
+const NEWS_JP2 = 'イギリスのオフィシャルチャートにBTSのジョングクの曲が大幅に含まれています...Stray Kidsも初ランクイン！\nイギリスのオフィシャルチャートにBTSのジョングクの曲が大幅に上昇しました。\n現地時間17日に公開されたオフィシャルシングルチャート「トップ100」を見ると、ジョングクのソロアルバム「Golden」のタイトル曲「スタンディング ネクスト トゥ ユー」が30位、「To Much」48位、「セブン」54位、「3D」75位など、4曲が含まれています。\nオフィシャルアルバムチャート「トップ100」にもジョングクのソロアルバム「Golden」が21位に上がっています。\nグループのStray Kidsも新しいアルバム「Rock Star」のタイトル曲「Rock」でオフィシャルシングルチャートの44位に初登場し、アルバムは69位を記録しています。\n世界で活動するDJ兼プロデューサーPeggy Gouの「It Gows Like (Nananana)」は70位で22週間「トップ100」の座を維持しています。'
+
+const NEWS_ID="BTS Jungkook 'Standing Next to You' mencapai peringkat 5 di tangga lagu Billboard!\n Anggota grup BTS, Jungkook, meraih peringkat 5 di tangga lagu utama Billboard Hot 100 dengan lagu utama dari album solonya yang dirilis pada tanggal 3.\n Billboard melalui situs resminya pada tanggal 13 mengumumkan bahwa 'Standing Next to You' naik ke peringkat 5 setelah lagu-lagu seperti 'Cruel Summer' oleh Taylor Swift.\n Billboard menyebutnya sebagai 'rekam solonya Jungkook yang ketiga yang mencapai 10 besar' dan mengatakan bahwa lagu ini juga meraih peringkat 1 dalam penjualan digital lagu.\n 'Standing Next to You' menjadi lagu utama dari album solonya yang berjudul 'Golden', lagu ini memiliki genre retro punk dengan vokal Jungkook yang terasa ritmis. Lagu ini memiliki ciri khas dengan permainan alat musik tiup yang kaya dan suara perkusi yang kuat.\n Produser dari hit Jungkook 'Euphoria', Andrew Watt dan Circuit, juga berpartisipasi dalam lagu ini.\n Hot 100 adalah tangga lagu yang menggabungkan data streaming, penilaian radio, dan penjualan lagu secara keseluruhan di Amerika Serikat, dan memiliki arti yang besar di antara tangga lagu Billboard lainnya.\n 'Standing Next to You' mencatatkan 16 juta streaming, 400 ribu penilaian radio, dan 99 ribu penjualan fisik dan digital.\n Lagu ini juga mencapai peringkat 1 di tangga lagu Billboard Global 200 dan Billboard Global.\nLagu solonya Jungkook lainnya, 'Euphoria' dan '3D', menduduki peringkat 2 dan 9 di tangga lagu Billboard Global 200, serta peringkat 2 dan 5 di tangga lagu Billboard Global."
+
+const NEWS_ID2='Lagu Jungkook dari BTS termasuk dalam chart resmi Inggris... Stray Kids juga untuk pertama kalinya masuk!\nDi chart singel resmi Inggris, lagu solo Jungkook dari BTS telah naik secara signifikan.\nDalam "Top 100" chart singel resmi yang dirilis pada tanggal 17 waktu setempat, lagu utama dari album solo Jungkook yang berjudul "Still With You" berada di posisi 30, "Too Much" di posisi 48, "Seven" di posisi 54, dan "Euphoria" di posisi 75.\nAlbum solo Jungkook yang diberi judul "Golden" juga mencapai posisi 21 dalam chart album resmi "Top 100".\nGrup Stray Kids juga mendapatkan kesuksesan dengan lagu utama dari album baru mereka yang berjudul "Back Door" yang berhasil masuk ke posisi 44 dalam chart singel resmi, sedangkan album mereka mendapatkan posisi 69.\nDJ dan produser yang berkecimpung di panggung dunia, Peggy Gou, dengan lagu "(It Goes) Nananana" telah mempertahankan posisi 70 dalam chart "Top 100" selama 22 minggu.'
+
+
+const NEWS_LIST =[
+    {
+        id:"1",
+        ttle:"BTS 정국 '스탠딩 넥스트 투 유' 빌보드 싱글 5위",
+        dttm : '2023년 11월 15일',
+        journalis: "OBS경인TV",
+        com: 'OBS',
+        cate: "방송_연예",
+        content: "BTS 정국 '스탠딩 넥스트 투 유' 빌보드 싱글 5위\n 그룹 방탄소년단 BTS의 정국이 지난 3일 발표한솔로 앨범 타이틀곡으로 미국 빌보드 메인 싱글 차트 '핫 100'에서 5위를 차지했습니다.\n빌보드는 현지시간 13일 공식 홈페이지를 통해 '스탠딩 넥스트 투 유'가 테일러 스위프트의 '크루얼 서머' 등에 이어 5위에 올랐다고 밝혔습니다.\n 빌보드는 '정국의 세 번째 솔로 톱 10 기록'이라며 '이 곡은 디지털 송 세일즈에서 1위를 차지했다'고 소개했습니다.\n '스탠딩 넥스트 투 유'는 정국의 솔로 앨범 '골든'의 타이틀곡으로, 정국의 리듬감 있는 보컬이 돋보이는 레트로 펑크 장르의 곡입니다.\n 풍성한 관악기 연주와 퍼커션 사운드, 파워풀한 가창이 특징입니다.\n 정국의 히트곡 '세븐'의 프로듀서 앤드루 와트와 서킷이 참여했습니다.\n 핫 100은 미국 스트리밍 데이터, 라디오 방송 점수, 판매량 데이터를 종합해 순위가 산출되는 차트로, 빌보드 세부 차트 가운데 가장 의미가 큽니다.\n '스탠딩 넥스트 투 유'는 1천60만 스트리밍, 40만 라디오 방송 점수, 9만9천 실물ㆍ디지털 싱글 판매량을 기록했습니다.\n 이 노래는 빌보드 차트 '글로벌 200'과 '글로벌'에서도 각각 1위를 기록했습니다.\n 정국의 솔로곡 '세븐'과 '3D'는 '글로벌 200'에서 각각 2위와 9위, '글로벌'에서 2위와 5위에 들었습니다.."
+    },
+    {
+        id:"2",
+        ttle:"영국 오피셜 차트에 BTS 정국 곡 대거 포함...스트레이 키즈도 첫 진입",
+        dttm : '2023년 11월 18일',
+        journalis:'김정회 기자',
+        com: "YTN",
+        cate: "방송_연예",
+        content: "영국 오피셜 차트에 BTS 정국 곡 대거 포함...스트레이 키즈도 첫 진입!\n영국 오피셜 차트에 BTS의 정국의 곡이 대거 올랐습니다.\n현지 시간 17일 공개된 오피셜 싱글 차트 '톱 100'을 보면 정국의 솔로 앨범 '골든'의 타이틀 곡 '스탠딩 넥스트 투 유'가 30위, '투 머치' 48위, '세븐' 54위, '3D' 75위 등 네 곡이 포함됐습니다.\n오피셜 앨범 차트 '톱 100'에도 정국의 솔로 앨범 '골든'은 21위에 올라 있습니다.\n그룹 스트레이 키즈도 새 앨범 '락 스타'의 타이틀 곡 '락'으로 오피셜 싱글 차트 44위에 처음 올랐고 음반은 69위를 기록하고 있습니다.\n세계를 무대로 활동하는 DJ 겸 프로듀서 페기 구의 '(잇 고스 라이크) 나나나'는 70위로 22주째 '톱 100' 자리를 유지하고 있습니다."
+    },
+    {
+        id:"3",
+        ttle:"고디바, BTS 정국 솔로앨범 'GOLDEN'과 콜라보 진행",
+        dttm : '2023년 11월 16일',
+        journalis:'김태열 기자',
+        com: "헤럴드경제",
+        cate: "금융_재테크",
+        disable: true
+    },
+    {
+        id:"4",
+        ttle:"BTS 정국 '골든' 빌보드 2위…K팝 솔로 최고 판매량",
+        dttm : '2023년 11월 13일',
+        journalis:'OBS경인TV',
+        com: "OBS",
+        cate: "방송_연예",
+        disable: true
+    },
+]
 
 export default {
     NEWS_KR,
+    NEWS_KR2,
     NEWS_JP,
-    NEWS_ID
+    NEWS_JP2,
+    NEWS_ID,
+    NEWS_LIST,
+    NEWS_ID2
 }
